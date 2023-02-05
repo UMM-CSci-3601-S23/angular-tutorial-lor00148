06c38131aef4ef306fbf95392c034840b2ee6b80
Alwin Lor <lor00148@morris.umn.edu>
2023-02-05 14:30:58 -0600

    Added the ability to view product details
    Added pathing and defined what a product is

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit { 
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    //Get product ID from current route
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number (routeParams.get('productId'));
+
+    //Find the product that corresponds with the id provided
+    this.product = products.find(product => product.id ===productIdFromRoute)
+  }
+  
 }
